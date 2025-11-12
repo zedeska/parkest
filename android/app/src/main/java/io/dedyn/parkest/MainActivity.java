2d69277c475133bb1c9444be48e3284122f27317
zedeska <zedeska@protonmail.ch>
2025-11-12 20:20:28 +0100

    init

---
+++ android/app/src/main/java/io/dedyn/parkest/MainActivity.java
@@ -0,0 +1,5 @@
+package io.dedyn.parkest;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.dedyn.parkest',
+  appName: 'parkest',
+  webDir: 'dist'
+};
+
+export default config;
