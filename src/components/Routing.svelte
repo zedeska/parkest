b14def4c41df5b818153e747b3f02625688c73de
zedeska <zedeska@protonmail.ch>
2025-11-24 00:42:43 +0100

    responsive

--- src/components/Routing.svelte
+++ src/components/Routing.svelte
@@ -7,10 +7,10 @@
     }
 </script>
 
-<div class="absolute top-4 left-1/2 -translate-x-1/2 bg-white z-50 flex flex-col items-center justify-center p-4 rounded-lg shadow-lg w-1/6 max-w-md">
-    <button class="absolute right-4 top-4 cursor-pointer bg-transparent border-none" on:click={close} aria-label="Close">
-        <i class="fa-solid fa-xmark text-2xl"></i>
+<div class="absolute top-4 left-1/2 -translate-x-1/2 bg-white z-50 flex flex-col items-center justify-center p-4 rounded-lg shadow-lg md:w-1/6 w-3/6 max-w-md">
+    <button class="absolute md:right-4 md:top-4 right-1 top-1 cursor-pointer bg-transparent border-none" on:click={close} aria-label="Close">
+        <i class="fa-solid fa-xmark md:text-2xl text-lg"></i>
     </button>
-    <h1 class="text-xl font-bold mb-4">Navigation vers</h1>
-    <h2 class="text-lg">{destination ? destination : 'Inconnu'}</h2>
+    <h1 class="md:text-xl text-lg text-center font-bold mb-4">Navigation vers</h1>
+    <h2 class="md:text-lg text-md">{destination ? destination : 'Inconnu'}</h2>
 </div>
--- src/routes/Params.svelte
+++ src/routes/Params.svelte
@@ -4,22 +4,22 @@
     
 </script>
 
-<div class="flex flex-col justify-center items-center border-2 border-blue-500 p-5 rounded-lg bg-white shadow-lg space-y-4">
+<div class="flex flex-col justify-center items-center border-2 border-blue-500 p-2 rounded-lg bg-white shadow-lg m-2 gap-5">
     
-    <h1 class="text-3xl font-bold">Paramètres</h1>
+    <h1 class="md:text-3xl text-2xl font-bold">Paramètres</h1>
 
-    <div class="flex flex-row gap-3">
-        <label for=""><h2 class="text-xl">Hauteur du véhicule (en centimètres) :</h2></label>
+    <div class="flex flex-row gap-3 justify-between">
+        <label for=""><h2 class="md:text-xl text-lg">Hauteur du véhicule (en centimètres) :</h2></label>
         <input type="number" placeholder="ex: 136" min="100" max="500" bind:value={$UserContent.hauteur}>
     </div>
 
-    <div class="flex flex-row gap-3">
-        <label for=""><h2 class="text-xl">PMR (Mobilité réduite) :</h2></label>
+    <div class="flex flex-row gap-3 justify-between">
+        <label for=""><h2 class="md:text-xl text-lg">PMR (Mobilité réduite) :</h2></label>
         <input type="checkbox" name="" id="" bind:checked={$UserContent.pmr}/>
     </div>
     
-    <div class="flex flex-row gap-3">
-        <label for=""><h2 class="text-xl">Uniquement parking avec disponibilité :</h2></label>
+    <div class="flex flex-row gap-3 justify-between">
+        <label for=""><h2 class="md:text-xl text-lg">Uniquement parking avec disponibilité :</h2></label>
         <input type="checkbox" name="" id="" bind:checked={$UserContent.dspOnly}/>
     </div>
 </div>
